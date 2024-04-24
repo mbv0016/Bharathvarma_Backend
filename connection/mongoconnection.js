437475077324b72e64b47fca772902e021da88d2
Bharath Varma Mudundi <82640780+mbv0016@users.noreply.github.com>
2024-04-24 14:28:18 -0400

    first commit

---
+++ connection/mongoconnection.js
@@ -0,0 +1,6 @@
+module.exports = {
+    connectionurl: 'mongodb+srv://bharathvarma04:Mudundibv54@cluster0.ehq50iq.mongodb.net/?retryWrites=true&w=majority',
+    database: 'fashions',
+    collection: 'fashions_collecion'
+  };
+  
